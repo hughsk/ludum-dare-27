41d99dc6b01b49236c197d1170f1a10dec5d40be
Hugh Kennedy <hughskennedy@gmail.com>
2013-08-24 16:00:23 +1000

    rolling/tumbling

--- entities/player.js
+++ entities/player.js
@@ -2,7 +2,9 @@ var Box2D = require('box2dweb-commonjs').Box2D
 var b2Player = require('box2d-player')(Box2D)
 var bs = require('bindlestiff')
 
+var tau = Math.PI * 2
 var round = Math.round
+var abs = Math.abs
 var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
 var b2FixtureDef = Box2D.Dynamics.b2FixtureDef
 var b2BodyDef = Box2D.Dynamics.b2BodyDef
@@ -25,6 +27,9 @@ var player = bs.component('player')
     def.fixedRotation = true
 
     this.body = this.world.CreateBody(def)
+    this.rotation = 0
+    this.rotating = false
+    this.lastangle = 1
 
     var fixdef = new b2FixtureDef
     fixdef.shape = new b2CircleShape(0.49)
@@ -40,22 +45,44 @@ var player = bs.component('player')
     this.b2Pos = this.body.m_xf.position
   })
   .on('tick', function() {
-    this.body.m_linearVelocity.x =
+    var xspd = this.body.m_linearVelocity.x =
         this.controls.left  ? -14
       : this.controls.right ? +14
       : 0
 
+    if (this.rotating) {
+      this.rotation += xspd > 0
+        ? +0.18
+        :  xspd < 0
+        ? -0.18
+        : this.lastangle > 0
+        ? +0.18
+        : -0.18
+      this.lastangle = xspd || this.lastangle
+    } else {
+      this.rotation = 0
+    }
+
     if (this.controls.jump) this.b2p.jump()
+    this.rotating = ((abs(this.body.m_linearVelocity.y)) > 0.2)
 
     tempPosition[0] = round(this.b2Pos.x)
     tempPosition[1] = round(this.b2Pos.y)
     this.game.field.move(tempPosition)
   })
   .on('draw', function(ctx, game) {
+    var x = this.b2p.body.m_xf.position.x * 30 - game.camera.pos[0]
+    var y = this.b2p.body.m_xf.position.y * 30 - game.camera.pos[1]
+    ctx.save()
+    ctx.translate(x, y)
+    ctx.rotate(
+        this.body.m_linearVelocity.x > 0
+      ? this.rotation
+      : this.rotation
+    )
     ctx.fillStyle = '#FFCFBF'
-    var x = this.b2p.body.m_xf.position.x * 30 - 15 - game.camera.pos[0]
-    var y = this.b2p.body.m_xf.position.y * 30 - 15 - game.camera.pos[1]
-    ctx.fillRect(x, y, 30, 30)
+    ctx.fillRect(-15, -15, 30, 30)
+    ctx.restore()
   })
 
 module.exports = bs.define()
